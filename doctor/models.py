eb5ecfbee0e4e4bea696deb474e01faf8f8174bb
rajon-git <rajon.zhsust15@gmail.com>
2024-09-20 22:13:56 +0600

    Specialization model create

--- doctor/models.py
+++ doctor/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class Specialization(models.Model):
+    name = models.CharField(max_length=30)
+    slug = models.SlugField(max_length=40)
