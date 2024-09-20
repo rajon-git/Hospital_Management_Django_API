4f4a92c25386ed8f856b6525912a792b7ad59c30
rajon-git <rajon.zhsust15@gmail.com>
2024-09-20 22:16:24 +0600

    AvailabelTime model create

--- doctor/models.py
+++ doctor/models.py
@@ -5,6 +5,18 @@ class Specialization(models.Model):
     name = models.CharField(max_length=30)
     slug = models.SlugField(max_length=40)
 
+    def __str__(self):
+        return self.name
+
 class Designation(models.Model):
     name = models.CharField(max_length=30)
     slug = models.SlugField(max_length=40)
+
+    def __str__(self):
+        return self.name
+
+class AvailabelTime(models.Model):
+    name = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.name
