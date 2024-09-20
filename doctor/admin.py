6ca06d35ad18eb8646ab96c41f86d9730fc4db82
rajon-git <rajon.zhsust15@gmail.com>
2024-09-20 22:26:24 +0600

    admin site register for doctor apps

--- doctor/admin.py
+++ doctor/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Designation, Specialization, AvailableTime,Doctor
 
 # Register your models here.
+admin.site.register(Specialization)
+admin.site.register(Designation)
+admin.site.register(AvailableTime)
+admin.site.register(Doctor)
