8ada05a1151f0efaef37329bd5ee8f891f188e98
rajon-git <rajon.zhsust15@gmail.com>
2024-09-20 22:22:57 +0600

    doctor model create

--- doctor/models.py
+++ doctor/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Specialization(models.Model):
@@ -15,8 +16,21 @@ class Designation(models.Model):
     def __str__(self):
         return self.name
 
-class AvailabelTime(models.Model):
+class AvailableTime(models.Model):
     name = models.CharField(max_length=100)
     
     def __str__(self):
         return self.name
+    
+class Doctor(models.Model):
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='doctor/images/')
+    designation = models.ManyToManyField(Designation)
+    specialization = models.ManyToManyField(Specialization)
+    available_time = models.ForeignKey(AvailableTime, on_delete=models.CASCADE)
+    fee = models.IntegerField()
+    meet_link = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"Dr. {self.user.first_name} {self.user.last_name}"
