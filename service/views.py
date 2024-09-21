588fd63606ff47e88e8e6f5858d7d7ebd7c7fba3
rajon-git <rajon.zhsust15@gmail.com>
2024-09-21 10:05:35 +0600

    service views create

--- service/views.py
+++ service/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Service
+from .serializers import ServceSerializer
 
 # Create your views here.
+class ServiceViewset(viewsets.ModelViewSet):
+    queryset = Service.objects.all()
+    serializer_class = ServceSerializer 
