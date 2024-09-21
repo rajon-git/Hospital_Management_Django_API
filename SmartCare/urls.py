ad7194d54f115f922756ebea0d8e92f2b979f4de
rajon-git <rajon.zhsust15@gmail.com>
2024-09-21 10:01:23 +0600

    service serializers create

--- SmartCare/urls.py
+++ SmartCare/urls.py
@@ -5,7 +5,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('contact_us.urls')),
+    path('s', include('contact_us.urls')),
 ]
 urlpatterns += static(settings.MEDIA_URL,
                           document_root=settings.MEDIA_ROOT)
