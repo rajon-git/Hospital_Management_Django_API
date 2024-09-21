a68c919546a7a65bbdc479a3b534aaa33c1d8418
rajon-git <rajon.zhsust15@gmail.com>
2024-09-21 22:31:57 +0600

    appointment views create

--- appointment/views.py
+++ appointment/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import AppointmentSerializer
+from .models import Appointment
 
 # Create your views here.
+class AppointmentViewSet(viewsets.ModelViewSet):
+    queryset = Appointment.objects.all()
+    serializer_class = AppointmentSerializer
