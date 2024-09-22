aefb118ebe254c0f435be5198a92288dcf5ca078
rajon-git <rajon.zhsust15@gmail.com>
2024-09-22 22:49:24 +0600

    doctor page pagination create

--- doctor/views.py
+++ doctor/views.py
@@ -4,11 +4,18 @@ from .models import Doctor, Specialization, Designation, AvailableTime, Review
 from .serializers import DoctorSerializer, SpecializationSerializer, DesignationSerializer, AvailableTimeSerializer, ReviewSerializer
 from rest_framework.permissions import IsAuthenticated,IsAuthenticatedOrReadOnly
 from rest_framework.permissions import BasePermission
+from rest_framework import filters, pagination
 
 # Create your views here.
+class DoctorPagination(pagination.PageNumberPagination):
+    page_size = 1 # items per page
+    page_size_query_param = page_size
+    max_page_size = 100
+
 class DoctorViewSet(viewsets.ModelViewSet):
     queryset = Doctor.objects.all()
     serializer_class = DoctorSerializer
+    pagination_class = DoctorPagination
 
 class SpecializationViewSet(viewsets.ModelViewSet):
     queryset = Specialization.objects.all()
