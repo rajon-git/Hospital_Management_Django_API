9df894d7928bbcaf82c522f7a47c197830bbdad9
rajon-git <rajon.zhsust15@gmail.com>
2024-09-22 12:37:15 +0600

    sent email for confirmation to register

--- patient/views.py
+++ patient/views.py
@@ -4,6 +4,13 @@ from .serializers import PatientSerializer, RegistrationSerializer
 from rest_framework import viewsets
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from django.contrib.auth.tokens import default_token_generator
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.utils.encoding import force_bytes
+
+# for sending email
+from django.core.mail import EmailMultiAlternatives
+from django.template.loader import render_to_string
 
 # Create your views here.
 
@@ -19,4 +26,14 @@ class UserRegistrationApiView(APIView):
 
         if serializer.is_valid():
             user = serializer.save()
+            token = default_token_generator.make_token(user)
+            uid = urlsafe_base64_encode(force_bytes(user.pk))
+            confirm_link = f"http://127.0.0.1:8000/patient/activate/{uid}/{token}/"
+            email_subject = "Confirm Your Account"
+            email_body = render_to_string('confirm_email.html',{'confirm_link': confirm_link})
+
+            email = EmailMultiAlternatives(email_subject,'',to=[user.email])
+            email.attach_alternative(email_body,"text/html")
+            email.send()
+            return Response("Check your email for confirmation")
         return Response(serializer.errors)
