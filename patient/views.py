ba5298d41133cda9f06be0c6183fa880e2be4a84
rajon-git <rajon.zhsust15@gmail.com>
2024-09-22 11:10:22 +0600

    user registration view create

--- patient/views.py
+++ patient/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 from .models import Patient
-from .serializers import PatientSerializer
+from .serializers import PatientSerializer, RegistrationSerializer
 from rest_framework import viewsets
+from rest_framework.views import APIView
 
 # Create your views here.
 
 class PatientViewSet(viewsets.ModelViewSet):
     queryset = Patient.objects.all()
     serializer_class = PatientSerializer
+
+class UserRegistrationApiView(APIView):
+    serializer_class = RegistrationSerializer
