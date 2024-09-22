1745e759fe1a6c38a2043986cee6b435aea3d5fd
rajon-git <rajon.zhsust15@gmail.com>
2024-09-22 22:58:50 +0600

    filter doctor available time

--- doctor/views.py
+++ doctor/views.py
@@ -25,11 +25,18 @@ class DesignationViewSet(viewsets.ModelViewSet):
     queryset = Designation.objects.all()
     serializer_class = DesignationSerializer
 
+class AvailableTimeForSpecificDoctor(filters.BaseFilterBackend):
+    def filter_queryset(self, request, query_set, view):
+        doctor_id = request.query_params.get("doctor_id")
+        if doctor_id:
+            return query_set.filter(doctor = doctor_id)
+        return query_set
 
 class AvailableTimeViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticated]
     queryset = AvailableTime.objects.all()
     serializer_class = AvailableTimeSerializer
+    filter_backends = [AvailableTimeForSpecificDoctor]
     
     
 
